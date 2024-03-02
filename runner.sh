c6e0ef95eefff0a9626d4a086d76558f6c038955
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2024-03-02 13:15:10 +0600

    Add exact instances

--- runner.sh
+++ runner.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# check if 1 argument is given
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 exact || heuristic"
+    exit 1
+fi
 
-mkdir -p output_logs
+outdir="output-logs-$1"
+
+mkdir -p $outdir
 
 # Iterate over all unzipped instances
 for file in unzipped/*; do
     echo "Running instance $(basename $file)..."
     # Run "./flow_cutter_pace17 < $file" in the background
-    ./flow_cutter_pace17 < $file > output_logs/$(basename $file).log 2>&1 &
+    ./flow_cutter_pace17 < $file > $outdir/$(basename $file).log 2>&1 &
     pid=$!
 
     # Sleep for 60s (change here if you want to run for more/less time)
-    sleep 60
+    sleep 5
 
     # Check if the process is still running and send SIGTERM
     if kill -0 $pid 2> /dev/null; then
--- unzipper.sh
+++ unzipper.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-if [ -d "unzipped" ]; then
+# Check if one argument is given
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 zipped-instances-dir"
+    exit 1
+fi
+
+# Check if input directory exists
+if [ ! -d "$1" ]; then
+    echo "Directory $1 does not exist"
+    exit 1
+fi
+
+unzippedDir="unzipped-$1"
+# If already unzipped, exit
+if [ -d $unzippedDir ]; then
     exit 0
 fi
 
-mkdir unzipped
+mkdir $unzippedDir
 
 # Unzip all input instances *.xz in unzipped/ directory
 echo "Unzipping files..."
-for file in heuristic-tw-instances/*.xz; do
-    unxz -c $file > unzipped/$(basename $file .xz)
+for file in $1/*.xz; do
+    unxz -c $file > $unzippedDir/$(basename $file .xz)
     echo "Unzipped $(basename $file .xz)"
 done
 echo "------------------Unzipping done---------------------"
